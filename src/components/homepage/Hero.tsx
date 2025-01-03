ddd4692a88801ebf45ce86a24e2914f32b997373
CakraDarma <baguscakraa02@gmail.com>
2025-01-04 00:49:03 +0800

    feat: hero

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -1,6 +1,7 @@
 import { cx } from "class-variance-authority";
 import React from "react";
 import GradientImage from "../ui/GradientImage";
+import Button from "../ui/button";
 
 export default function Hero() {
   return (
@@ -11,14 +12,24 @@ export default function Hero() {
         "fullscreen"
       )}
     >
-      <p
-        className={cx(
-          "absolute bottom-120-d z-10 landscape:min-lg:text-64-d text-white landscape:min-lg:w-[42%] tracking-tighter leading-tight left-128-d",
-          "text-40-m font-prata"
-        )}
-      >
-        Discover Borneo’s Hidden Heart
-      </p>
+      <div className="absolute inset-0 bg-black bg-opacity-20 z-10"></div>
+
+      <div className="absolute z-20 text-white landscape:min-lg:w-[50%] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 flex flex-col gap-10">
+        <p className={cx("text-14-d text-center font-semibold")}>
+          Borneo river cruise
+        </p>
+        <p className={cx("text-80-d leading-tight font-prata text-center")}>
+          Discover Borneo’s Hidden Heart
+        </p>
+        <p className={cx("text-20-d leading-tight text-center")}>
+          Immerse yourself in the beauty of Borneo’s rivers, where each journey
+          unveils the stories of the land, wildlife, and people.
+        </p>
+        <Button className="text-18-d   hover:shadow-lg hover:scale-105 transition w-fit mx-auto">
+          Explore Our Cruises
+        </Button>
+      </div>
+
       <GradientImage src="/images/hero.jpg" />
     </div>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,5 +1,6 @@
 import React, { ButtonHTMLAttributes, ReactNode } from "react";
 import clsx from "clsx";
+import { cn } from "@/lib/utils";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
@@ -14,13 +15,13 @@ export default function Button({
   ...props
 }: ButtonProps) {
   const variantStyles = {
-    primary: "bg-primary text-white hover:bg-[#A01D14] focus:ring-blue-400",
+    primary: "bg-[#CD5A05] text-white hover:bg-[#E67017] focus:ring-blue-400",
     secondary: "bg-secondary text-white", // Style untuk secondary
   };
 
   return (
     <button
-      className={clsx(
+      className={cn(
         "landscape:min-lg:px-40-d landscape:min-lg:py-12 rounded-full focus:outline-none focus:ring-0 landscape:min-lg:text-18-d",
         "text-12-m px-16-m py-[0.93vw]",
         variantStyles[variant],
