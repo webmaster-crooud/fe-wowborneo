c61e3dbd84add8598d76bca65f0e4217466239fa
Mikael Aditya N <mikaeladityan.99@gmail.com>
2025-06-04 11:24:44 +0700

    refact: Update

--- src/components/explore/detail/TravelTips.tsx
+++ src/components/explore/detail/TravelTips.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { cx } from "class-variance-authority";
 import GradientImage from "../../ui/GradientImage";
@@ -7,51 +7,37 @@ import CardWhyChoose from "@/components/homepage/CardWhyChoose";
 import { useState } from "react";
 
 export default function TravelTips() {
-  const [expandedIndex, setExpandedIndex] = useState<null | number>(null)
-  const datas = [
-    {
-      head: "Getting There",
-      desc: "Sebangau is accessible by road from Palangkaraya, with guided tours available for deeper exploration.",
-      img: "/images/people/family.png",
-    },
-    {
-      head: "Best Time to Visit",
-      desc: "Sebangau is accessible by road from Palangkaraya, with guided tours available for deeper exploration.",
-      img: "/images/people/family2.png",
-    },
-    {
-      head: "Conservation Etiquette",
-      desc: "Sebangau is accessible by road from Palangkaraya, with guided tours available for deeper exploration.",
-      img: "/images/wildlife/orangutan6.png",
-    },
-  ];
-  return (
-    <Container className='space-y-20'>
-      <div className="space-y-4 lg:flex lg:items-end lg:gap-6">
-        <p className={cx("font-prata text-subtitle")}>
-          Travel Tips for Visiting Sebangau National Park
-        </p>
-        <p className="text-description">
-          Planning a visit to Sebangau? Here are some essential tips to enhance your adventure in this beautiful park. By following these tips, you’ll help protect Sebangau’s delicate ecosystem while having a memorable experience         </p>
-      </div>
-      <div className={cx("pt-20 grid gap-4 transition-all duration-500 grid-cols-1 lg:grid-cols-12")}>
-        {datas.map((data, index) => {
-          const colSpan = expandedIndex === null
-            ? "lg:col-span-4"
-            : (expandedIndex === index ? "lg:col-span-6" : "lg:col-span-3");
+	const [expandedIndex, setExpandedIndex] = useState<null | number>(null);
+	const datas = [
+		{
+			title: "Getting There",
+			description: "Sebangau is accessible by road from Palangkaraya, with guided tours available for deeper exploration.",
+			cover: "/images/people/family.png",
+		},
+		{
+			title: "Best Time to Visit",
+			description: "Sebangau is accessible by road from Palangkaraya, with guided tours available for deeper exploration.",
+			cover: "/images/people/family2.png",
+		},
+		{
+			title: "Conservation Etiquette",
+			description: "Sebangau is accessible by road from Palangkaraya, with guided tours available for deeper exploration.",
+			cover: "/images/wildlife/orangutan6.png",
+		},
+	];
+	return (
+		<Container className="space-y-20">
+			<div className="space-y-4 lg:flex lg:items-end lg:gap-6">
+				<p className={cx("font-prata text-subtitle")}>Travel Tips for Visiting Sebangau National Park</p>
+				<p className="text-description">Planning a visit to Sebangau? Here are some essential tips to enhance your adventure in this beautiful park. By following these tips, you’ll help protect Sebangau’s delicate ecosystem while having a memorable experience </p>
+			</div>
+			<div className={cx("pt-20 grid gap-4 transition-all duration-500 grid-cols-1 lg:grid-cols-12")}>
+				{datas.map((data, index) => {
+					const colSpan = expandedIndex === null ? "lg:col-span-4" : expandedIndex === index ? "lg:col-span-6" : "lg:col-span-3";
 
-          return (
-            <CardWhyChoose
-              colSpan={colSpan}
-              key={index}
-              item={data}
-              isOpen={expandedIndex === index}
-              onClick={() => setExpandedIndex((prev) => (prev === index ? null : index))}
-              height="h-full"
-            />
-          );
-        })}
-      </div>
-    </Container>
-  );
+					return <CardWhyChoose colSpan={colSpan} key={index} item={data} isOpen={expandedIndex === index} onClick={() => setExpandedIndex((prev) => (prev === index ? null : index))} height="h-full" />;
+				})}
+			</div>
+		</Container>
+	);
 }
